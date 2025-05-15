b11b008ba7044df0f36202f81d850ea8e1622e4e
Yur4Arkhipov <yura4rkhipov@yandex.ru>
2025-05-15 17:09:05 +0300

    init

---
+++ app/src/main/java/com/example/courses/MainActivity.kt
@@ -0,0 +1,52 @@
+package com.example.courses
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.courses.ui.theme.CoursesTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            CoursesTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    OnboardingScreen(
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+
+@Composable
+fun OnboardingScreen(
+    modifier: Modifier = Modifier
+) {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier.fillMaxSize()
+    ) {
+        Text(
+            text = stringResource(R.string.onboarding_description),
+            fontFamily = FontFamily(Roboto)
+        )
+    }
+}
---
+++ app/src/main/java/com/example/courses/ui/theme/Fonts.kt
@@ -0,0 +1,21 @@
+package com.example.courses.ui.theme
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import com.example.courses.R
+
+val Roboto = FontFamily(
+    Font(
+        resId = R.font.roboto,
+        weight = FontWeight.W400
+    ),
+    Font(
+        resId = R.font.roboto,
+        weight = FontWeight.W500
+    ),
+    Font(
+        resId = R.font.roboto,
+        weight = FontWeight.W600
+    )
+)
